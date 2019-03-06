14f529439a559fb3c3c0ac5f607664e3b826e758
v86861062 <v86861062@gmail.com>
2019-03-06 19:51:22 +0800

    Updates

---
+++ precache-manifest.ae2d8e30fafb102d6f248be2c4d69621.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a458352e59020494010",
+    "url": "/weather-forecast/static/css/main.0d5a2d20.chunk.css"
+  },
+  {
+    "revision": "9a458352e59020494010",
+    "url": "/weather-forecast/static/js/main.9a458352.chunk.js"
+  },
+  {
+    "revision": "dfe4dd73f32251f620c1",
+    "url": "/weather-forecast/static/js/runtime~main.dfe4dd73.js"
+  },
+  {
+    "revision": "ae10d0bfeab009486505",
+    "url": "/weather-forecast/static/css/2.1bcecb18.chunk.css"
+  },
+  {
+    "revision": "ae10d0bfeab009486505",
+    "url": "/weather-forecast/static/js/2.ae10d0bf.chunk.js"
+  },
+  {
+    "revision": "fcf32c41e3fab9e32d079b10257cb6da",
+    "url": "/weather-forecast/static/media/icomoon.fcf32c41.woff"
+  },
+  {
+    "revision": "f4ae08df9d0190fb67118b5cf6ae7205",
+    "url": "/weather-forecast/static/media/icomoon.f4ae08df.eot"
+  },
+  {
+    "revision": "9e94e87c2697827f980d0d5114c5727b",
+    "url": "/weather-forecast/static/media/icomoon.9e94e87c.ttf"
+  },
+  {
+    "revision": "36ba6e5214740636fb88d844257c9747",
+    "url": "/weather-forecast/static/media/icomoon.36ba6e52.svg"
+  },
+  {
+    "revision": "cb93c15e6d5df53403bd79ca42fdef5f",
+    "url": "/weather-forecast/static/media/bk.cb93c15e.jpeg"
+  },
+  {
+    "revision": "ced720eee7dc0f54cb94791e6e9137f1",
+    "url": "/weather-forecast/index.html"
+  }
+];
