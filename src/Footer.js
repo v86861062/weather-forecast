902e748bb290445ba14f3e4c611e750566afc78c
v86861062 <v86861062@gmail.com>
2019-03-06 09:34:01 +0800

    Add image source in Footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,15 @@ import "tachyons/css/tachyons.min.css"
 function Footer() {
   return (
     <footer>
-      <a className="ma2 db f6 link dim gray" href="https://darksky.net/dev">
+      <a className="fr ma2 db f6 link dim gray" href="https://darksky.net/dev">
         Powered by Dark Sky
+      </a>
+
+      <a
+        className="fl ma2 db f6 link dim gray"
+        href="https://www.pexels.com/photo/landscape-photo-of-mountain-669963/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels"
+      >
+        Photo by Tsang Chung Yee from Pexels
       </a>
     </footer>
   )
